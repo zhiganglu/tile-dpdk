578e65113609409e1f6fd18e9ce6233d3c0e66b4
Zhigang Lu <zlu@tilera.com>
2014-11-12 15:41:38 +0800

    eal: Enable iopl operaion for x86 only
    The iopl() is x86 specific, so only enable it for x86.

--- lib/librte_eal/linuxapp/eal/eal.c
+++ lib/librte_eal/linuxapp/eal/eal.c
@@ -50,7 +50,10 @@
 #include <errno.h>
 #include <sys/mman.h>
 #include <sys/queue.h>
+
+#if defined(RTE_ARCH_X86_64) || defined(RTE_ARCH_I686)
 #include <sys/io.h>
+#endif
 
 #include <rte_common.h>
 #include <rte_debug.h>
@@ -756,9 +759,14 @@ rte_eal_mcfg_complete(void)
 int
 rte_eal_iopl_init(void)
 {
+#if defined(RTE_ARCH_X86_64) || defined(RTE_ARCH_I686)
 	if (iopl(3) != 0)
 		return -1;
 	return 0;
+#else
+	return -1;
+
+#endif
 }
 
 /* Launch threads, called at application init(). */
